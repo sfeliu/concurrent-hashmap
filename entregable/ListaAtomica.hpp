42dad5cc362566af34feb04077c8719102cdd068
nico <nico@notebook.patricia>
2018-09-24 16:05:29 -0300

    Agregue std:: en push_front ahora compila

--- entregable/ListaAtomica.hpp
+++ entregable/ListaAtomica.hpp
@@ -33,7 +33,7 @@ public:
         nuevo->_next = _head.load();
         // Espero a que _head y nuevo->next sean iguales, es decir, que ningun
         // thread me meta en el medio y cambie l a_head de la lista
-        while (!atomic_compare_exchange_weak(&_head, nuevo->next, nuevo)) {}
+        while (!std::atomic_compare_exchange_weak(&_head, &nuevo->_next, nuevo)) {}
         // aca nuevo->next apunta a (lo que era) _head y por lo tanto no me perdi ningun
         // nodo
         // Consultar diferencia entre _weak y _strong.
